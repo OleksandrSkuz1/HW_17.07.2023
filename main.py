3a36198f93c2ea160283d4c24b6e11433c0a2b02
OleksandrSkuz1 <138138803+OleksandrSkuz1@users.noreply.github.com>
2023-07-23 12:53:20 +0300

    add

---
+++ main.py
@@ -0,0 +1,12 @@
+if __name__ == '__main__':
+
+    # ЗАВДАННЯ 1
+
+    # Напишіть функцію, яка відобразить на екрані такий форматований текст:
+    # "Don't compare yourself with anyone in this world…
+    # if you do so, you are insulting yourself."
+
+    def my_fn():
+        text = "Don't compare yourself with anyone in this world…if you do so, you are insulting yourself"
+        print(text)
+        my_fn()
